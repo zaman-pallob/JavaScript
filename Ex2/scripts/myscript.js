a050122d85af68602b9518e85c8f8f4caf2d05ea
Atikur Zaman Pallob <atikurzamanpallob@gmail.com>
2020-06-14 15:30:18 +0600

    ex2

---
+++ Ex2/scripts/myscript.js
@@ -0,0 +1,4 @@
+document.getElementById('display').innerHTML = "Each of the past three centuries was dominated by a single new technology In practice, the numbers to be sorted are rarely isolated values. Each is usually partof a collection of data called a record. Each record contains a key, which is thevalue to be sorted. The remainder of the record consists of satellite data, which are"
+document.getElementById('display').style.textAlign = "center"
+document.getElementById('display').style.fontSize = "20px"
+document.getElementById('display').style.color = "red"
